bb6445fc770fc9267b79a3eb62543759c1217070
dhiralpat <102880872+dhiralpat@users.noreply.github.com>
2022-04-16 15:35:14 -0500

    Add files via upload

---
+++ mycicd/src/test/java/test/com/sears/homeservice/EmployeeTest.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package test.com.sears.homeservice;
+
+import com.sears.homeservice.mycicd.Employee;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author dpate
+ */
+public class EmployeeTest {
+    Employee emp;
+    
+    public EmployeeTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testFirst() {
+        emp = new Employee(1, "tom");
+        assertEquals(1,emp.getId());
+        assertEquals("tom", emp.getFirst());
+    }
+}
